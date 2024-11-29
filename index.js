fbbce87f8c6a05178269c0522c3dae5041c53e90
livedder44 <60624850+livedder44@users.noreply.github.com>
2024-11-29 21:26:14 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+{
+  "name": "cessna-scraper",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "puppeteer": "^20.0.0"
+  }
+}
